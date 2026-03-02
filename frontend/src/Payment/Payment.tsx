c6e42f5e42d9858fee231a9f0196444c6a7ec184
YorkFan4925 <106598577+YorkFan4925@users.noreply.github.com>
2026-03-01 23:46:55 -0500

    Update Payment.tsx
    fix bugs

--- frontend/src/Payment/Payment.tsx
+++ frontend/src/Payment/Payment.tsx
@@ -2,11 +2,52 @@ import React from "react";
 import CheckoutSteps from "./CheckoutSteps";
 
 export default function Payment() {
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert("Payment processing...");
+  };
+
   return (
     <div>
       <CheckoutSteps step1 step2 step3 />
-      <h2>Payment</h2>
-      <p>Payment screen goes here.</p>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <ul className="form-container">
+            <li>
+              <h2>Payment</h2>
+            </li>
+
+            <li>
+              <div>
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="paypal"
+                  value="paypal"
+                  defaultChecked
+                />
+                <label htmlFor="paypal">Paypal</label>
+              </div>
+
+              <div>
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="bitcoin"
+                  value="bitcoin"
+                />
+                <label htmlFor="bitcoin">Bitcoin</label>
+              </div>
+            </li>
+
+            <li>
+              <button type="submit" className="button primary">
+                Continue
+              </button>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
   );
 }
