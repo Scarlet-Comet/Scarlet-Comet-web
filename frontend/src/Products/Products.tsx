f3c78704fce3637bc57e8e7fab34fa15545198e8
York Fan <fanyuke4925@gmail.com>
2026-04-21 17:15:25 -0400

    Update products to load from API

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,40 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  rating: number;
+  numReviews: number;
+  image: string;
+};
 
 const Products = () => {
-  const products = [
-    {
-      id: 1,
-      name: "Slim Shirt",
-      brand: "Nike",
-      price: 60,
-      rating: 4.5,
-      numReviews: 10,
-      image: "/images/d1.jpg",
-    },
-    {
-      id: 2,
-      name: "Fit Shirt",
-      brand: "Adidas",
-      price: 50,
-      rating: 4.0,
-      numReviews: 8,
-      image: "/images/d1.jpg",
-    },
-  ];
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Product[]>("http://localhost:5104/catalog", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <ul className="products">
       {products.map((product) => (
         <li key={product.id}>
           <div className="product">
-            <img
-              className="product-image"
-              src={product.image}
-              alt="product"
-            />
-            <div className="product-name">
-              {product.name}
-            </div>
+            <img className="product-image" src={product.image} alt="product" />
+            <div className="product-name">{product.name}</div>
             <div className="product-brand">{product.brand}</div>
             <div className="product-price">${product.price}</div>
             <div className="product-rating">
